dda73e8de4a317aea3855ef6840a91969a590ead
Jan McSorley <mcsorleyjan@gmail.com>
2023-05-28 14:03:37 -0500

    feat: Add querySelectors and eventListeners for buttons: clear, let's cook, and add recipe.

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-// ~~~~~~ query selectors:
+// ~~~~~~ button query selectors:
+var addRecipeButton = document.querySelector('#addRecipe');
+var letsCookButton = document.querySelector('#letsCook');
+var clearButton = document.querySelector('#clearAll');
+
+// ~~~~~~ form query selectors:
+var mealChoiceForm = document.querySelector('#mealChoices');
+
+// ~~~~~~ image query selectors:
+var cookPotImage = document.querySelector('#cookPot');
+
+// ~~~~~~ other query selectors:
+var shouldMakeDiv = document.querySelector('#shouldMake');
+var selectedMealItems = document.querySelector('#selectedItems');
 
 // ~~~~~~ food arrays:
 var sides = [
@@ -55,12 +68,34 @@ var desserts = [
 
 
 // ~~~~~~ event listeners
+window.addEventListener('load', defaultPage);
+addRecipeButton.addEventListener('click', addRecipe);
+letsCookButton.addEventListener('click', isSelected)
+clearButton.addEventListener('click', defaultPage)
 
 
 // ~~~~~~ functions
 function getRandomIndex(array) {
     return Math.floor(Math.random()) * array.length
 }
+
+function defaultPage () {
+    //on load and on click, all values will return to default - no radio buttons selected and cookPot image present
+}
+
+function isSelected () {
+    //check the radio button to see if it's selected.
+    //get a random index from the selected array
+    //hide the cookPot image
+    //show the "shouldMake" div
+
+}
+
+function addRecipe () {
+
+}
+
+
 
 
 
